e39e3c6fd56b6d4e4e407e5f5a54450d878cd3fc
abstraction <2830391724@qq.com>
2023-06-13 23:49:10 +0800

    初始化

--- src/test/java/com/example/onlinejudge/NormalTest.java
+++ src/test/java/com/example/onlinejudge/NormalTest.java
@@ -1,23 +1,19 @@
-//package com.example.abs;
-//
-//import com.example.abs.common.Result;
-//import com.example.abs.controller.BookController;
-//import com.example.abs.entity.Book;
-//import com.example.abs.service.BookService;
-//import com.example.abs.service.impl.BookServiceImpl;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//public class NormalTest {
-//    @Autowired
-//    private BookController bookController;
-//
-//    @Test
-//    void test01() {
-//        Result result = bookController.getById(111);
-//        System.out.println(result);
-//    }
-//}
+package com.example.onlinejudge;
+
+import com.example.onlinejudge.common.Result;
+import com.example.onlinejudge.controller.UserController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NormalTest {
+    @Autowired
+    private UserController userController;
+
+    @Test
+    void test01() {
+        Result result = userController.getById(111);
+        System.out.println(result);
+    }
+}
