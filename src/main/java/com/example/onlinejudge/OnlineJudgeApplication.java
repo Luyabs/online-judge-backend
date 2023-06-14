e1c45bae77226002bc0cab11df530c8ba150928a
abstraction <2830391724@qq.com>
2023-06-14 14:28:53 +0800

    ↑ 优化枚举类 简化方法使用 ↑ 完善登录注册 + 增加视图对象VO层 + 为所有controller增加基类继承(之后必须取消继承)

--- src/main/java/com/example/onlinejudge/OnlineJudgeApplication.java
+++ src/main/java/com/example/onlinejudge/OnlineJudgeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdvancedLearningApplication {
+public class OnlineJudgeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdvancedLearningApplication.class, args);
+        SpringApplication.run(OnlineJudgeApplication.class, args);
     }
 
 }
